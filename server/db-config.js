dcb471d1297b684898fb4494dd5a509809529366
Augusto Falcao <falcaoaugustos@gmail.com>
2018-01-01 19:59:41 -0300

    refactor (db config): update database connection to use process argument

--- server/db-config.js
+++ server/db-config.js
@@ -5,7 +5,7 @@ const knex = require('knex')({
     host : '127.0.0.1',
     user : process.argv[2],
     password : process.argv[3],
-    database : 'template1'
+    database : process.argv[4]
   }
 })
 
